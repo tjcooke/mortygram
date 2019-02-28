fee8d94ac4807916f308766ab1d66d9557b3ba72
Tyler Cooke <tylerstouchpad@gmail.com>
2019-02-28 11:59:28 -0500

    Add expand image function

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,14 @@ const targetElements = document.querySelectorAll("[data-target]");
 // we see an enlarged version of the image that 
 // is inside the .picture-frame
 
-function respondToClick() {
+function respondToClick(event) {
     console.log('hello');
     console.log('how are you?');
     console.log('I am fine.');
+
+    console.log(event.target.parentElement);
+    // add the "big" class to the thing that got clicked
+    event.target.parentElement.classList.add('big');
 }
 
 function attachClickHandler(oneElement) {
